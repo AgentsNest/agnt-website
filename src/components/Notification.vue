54e6a3554a24dab860d9146d007e781e7577004d
Vimal Bharti <s.vimalbharti@gmail.com>
2022-02-02 16:33:24 +0530

    Change Notification Fullscreen dailog

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -8,7 +8,7 @@
             </template>
         </v-snackbar>
 
-        <v-menu offset-y>
+        <!-- <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
                 <v-badge 
                     class="mr-2"
@@ -42,7 +42,55 @@
                     </v-list-item>
                 </v-list>
             </v-card>    
-        </v-menu>
+        </v-menu> -->
+
+        <v-dialog
+            v-model="dialog"
+            fullscreen
+            hide-overlay
+            transition="dialog-bottom-transition"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-badge 
+                    class="mr-2" dense bordered overlap color="error" 
+                    :content="unreadnotifications.length > 0 ? unreadnotifications.length : '0' " 
+                >
+                    <v-btn icon small color="grey darken-3" v-bind="attrs" v-on="on">
+                        <v-icon size="22" color="dark">mdi-bell</v-icon>
+                    </v-btn>
+                </v-badge>
+            </template>
+            <v-card tile>
+                <v-toolbar dark color="pink" tile>
+                    <v-btn icon dark @click="dialog = false">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                    <div class="title">Notifications</div>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                        <v-btn @click="markAsRead()" text>
+                            <v-icon>mdi-checkbox-marked-circle</v-icon>
+                            <span>Clear</span>
+                        </v-btn>
+                    </v-toolbar-items>
+                </v-toolbar>
+
+                <div class="grey lighten-4 pa-2 ">
+                    <div class="mb-2 py-3 px-3 box" v-for="notification in unreadnotifications" :key="notification.id">
+                        <div>
+                            <div class="body-2">
+                                <span class="pink--text">{{notification.data.Project}}</span> 
+                                <span class="grey--text text--darken-2 mx-1">Opened By</span> 
+                                <span class="black--text">{{notification.data.description}}</span>
+                            </div>
+                            <div class="body-2 grey--text text--darken-2">Duration: {{notification.data.duration}}</div>
+                            <div class="caption">{{notification.created_at | fromNow}}</div>
+                        </div>
+                    </div>
+                </div>
+
+            </v-card>
+        </v-dialog>
 
     </div>
 
@@ -55,7 +103,8 @@ export default {
     data(){
         return {
             notificationList: false,
-            snackbar: false
+            snackbar: false,
+            dialog: false,
         }
     },
     computed:{
@@ -69,6 +118,7 @@ export default {
             Notification.markAsRead().then(response => {
                 this.notificationList = false;
                 this.snackbar = true
+                this.$store.dispatch('unReadNotification');
             })
         }
     }
@@ -76,6 +126,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.box{
+    box-shadow: 0 2px 6px 0 rgba(136,148,171,.2),0 24px 20px -24px rgba(71,82,107,.1);
+    background: #fff;
+    border-radius: 8px;
+}
 </style>
